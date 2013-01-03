697fccc488a471c470e8efb72fcb7688c71520de
Florent LANTERNIER <ogame@lanternier.net>
2013-01-04 00:58:13 +0100

    Update Models
    Synchro pour faire voir à Pinpoint :)

---
+++ Core/Models/ship.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FllanBot\Core\Models;
+
+class Ship {
+	public $name;
+	public $speed;
+	public $attackStrength;
+	public $shieldStrength;
+	public $structuralIntegrity;
+	public $cargoCapacity;
+	public $fuelUsage;
+}
+
+?>
