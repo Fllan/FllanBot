64fa72e9f60f70720bc74e62033536b4017bc240
Maitre Sucre <maitre.sucre@yopmail.com>
2013-01-20 20:26:34 +0100

    Update

--- Core/Clients/AuthenticationClient.php
+++ Core/Clients/AuthenticationClient.php
@@ -16,8 +16,32 @@ class AuthenticationClient implements IAuthenticationClient {
         assert(!empty($profile->serverUrl));
         assert(!empty($profile->username));
 
-        echo sprintf("%s/game/reg/login2.php?login=%s&pass=%s&kid=&v=2",
-            $profile->serverUrl, $profile->username, $profile->password);
+        $url = self::buildAuthenticationEndpointUrl($profile);
+        $response = Request::get($url)
+            ->addHeaders(self::getDefaultHeaders())
+            ->expectsHtml()
+            ->send();
+
+        assert(!is_null($response));
+        assert(!empty($response->body));
+
+        return $response->body;
+    }
+
+    private static function buildAuthenticationEndpointUrl(Profile $profile) {
+        return sprintf("%s/game/reg/login2.php?login=%s&pass=%s&kid=&v=2",
+            rtrim($profile->serverUrl, '/'),
+            urlencode($profile->username),
+            urlencode($profile->password));
+    }
+
+    private static function getDefaultHeaders() {
+        return array(
+            'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Language' => 'fr-FR,fr;q=0.8,en-US;q=0.6,en;q=0.4',
+            'Connection' => 'close',
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.52 Safari/537.17))');
     }
 }
 
