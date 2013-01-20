48ca7ceb657a614967699867ea9c13dcf41520ed
Florent LANTERNIER <ogame@lanternier.net>
2013-01-20 18:29:02 +0100

    Update

--- Core/Clients/AuthenticationClient.php
+++ Core/Clients/AuthenticationClient.php
@@ -6,6 +6,8 @@ require_once '..\..\Dependencies\httpful.phar';
 
 use Httpful\Request;
 
-class authenticationClient {
-    public $
+class AuthenticationClient implements IAuthenticationClient {
+    $uri='http://uni111.ogame.us/game/reg/login2.php?login=THE+MUCKRAKER&pass=Branleur69%24&kid=&v=2';
 }
+
+?>
---
+++ Core/Clients/IAuthenticationClient.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FllanBot\Core\Clients;
+
+interface IAuthenticationClient {
+	public function authenticate($settings);
+}
+
+?>
