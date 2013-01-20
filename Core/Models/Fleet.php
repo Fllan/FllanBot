c150695cb858e1bfa1300a158b85e53fb091f580
Florent LANTERNIER <ogame@lanternier.net>
2013-01-20 11:08:35 +0100

    Update

--- Core/Models/Fleet.php
+++ Core/Models/Fleet.php
@@ -4,8 +4,11 @@ namespace FllanBot\Core\Models;
 
 class Fleet {
    public $arrivalTime;
-   public $departureTime;
+   public $arrivalDate;
+   public $origin; // coordinates of departure
+   public $target; // coordinates of target
    public $ships; // composition of the fleet (how many and what kind of ships)
+   public $player;
 }
 
 ?>
