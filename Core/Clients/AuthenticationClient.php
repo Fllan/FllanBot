6ce32deb552442983e0d53456cec9dd888e3773b
Maitre Sucre <maitre.sucre@yopmail.com>
2013-01-20 19:46:46 +0100

    Update

--- Core/Clients/AuthenticationClient.php
+++ Core/Clients/AuthenticationClient.php
@@ -11,6 +11,11 @@ use FllanBot\Core\Models\Profile;
 
 class AuthenticationClient implements IAuthenticationClient {
     public function authenticate(Profile $profile) {
+        assert(!is_null($profile));
+        assert(!empty($profile->password));
+        assert(!empty($profile->serverUrl));
+        assert(!empty($profile->username));
+
         echo sprintf("%s/game/reg/login2.php?login=%s&pass=%s&kid=&v=2",
             $profile->serverUrl, $profile->username, $profile->password);
     }
