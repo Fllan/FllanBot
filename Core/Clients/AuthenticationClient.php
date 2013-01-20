53f17099e8cd2a8b288b31885e204d73b35e4d80
Florent LANTERNIER <ogame@lanternier.net>
2013-01-20 18:45:42 +0100

    Update
    tab versus space

--- Core/Clients/AuthenticationClient.php
+++ Core/Clients/AuthenticationClient.php
@@ -9,10 +9,10 @@ use Httpful\Request;
 
 class AuthenticationClient implements IAuthenticationClient {
     public function authenticate ($settings) {
-		$url = 'http://uni111.ogame.us/game/reg/login2.php?login=THE+MUCKRAKER&pass=Branleur69%24&kid=&v=2';
-		echo $url;
-		$response = Request::get($url)->send();
-	}
+        $url = 'http://uni111.ogame.us/game/reg/login2.php?login=THE+MUCKRAKER&pass=Branleur69%24&kid=&v=2';
+        echo $url;
+        $response = Request::get($url)->send();
+    }
 }
 
 ?>
