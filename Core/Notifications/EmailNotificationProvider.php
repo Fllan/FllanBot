551f70be74436404c8b22ed637cb3d977d5b4445
Florent LANTERNIER <ogame@lanternier.net>
2013-02-16 23:52:59 +0100

    Update

--- Core/Notifications/EmailNotificationProvider.php
+++ Core/Notifications/EmailNotificationProvider.php
@@ -5,6 +5,8 @@ namespace FllanBot\Core\Notifications;
 require_once DIR_CORE . '/Models/Profile.php';
 require_once 'INotificationProvider.php';
 
+use FllanBot\Core\Models\Profile;
+
 class EmailNotificationProvider implements INotificationProvider {
     public function notify($message, Profile $profile) {
         $to = $profile->email;
--- Core/Notifications/INotificationProvider.php
+++ Core/Notifications/INotificationProvider.php
@@ -2,6 +2,10 @@
 
 namespace FllanBot\Core\Notifications;
 
+require_once DIR_CORE . '/Models/Profile.php';
+
+use FllanBot\Core\Models\Profile;
+
 interface INotificationProvider {
     public function notify($message, Profile $profile);
 }
